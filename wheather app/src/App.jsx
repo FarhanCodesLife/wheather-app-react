fa7dd4182c3975721020873b0f80713a8a106229
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-09-06 01:58:38 +0500

    loading

--- wheather app/src/App.jsx
+++ wheather app/src/App.jsx
@@ -4,11 +4,29 @@ import React, { useEffect, useRef, useState } from 'react'
 function App() {
 
   let [inputval,setinputval]=useState([])
-  let [wheatherdata,setwhetherdata]= useState(null)
+  let [wheatherdata,setwhetherdata]= useState([])
  
-  let val = useRef()
-    
+    async function getdata() {
+    try {
 
+        let data = await axios(`https://api.weatherapi.com/v1/current.json?key=6326e206cf9041e5b3853528241406&q=${val.current.value}&aqi=no`)
+        console.log(data);
+wheatherdata.push(data.data)
+setwhetherdata(wheatherdata)
+console.log(wheatherdata);
+
+// renderdata()
+
+      }
+     catch (error) {
+      console.log(error);
+      
+    }
+    
+    
+  }
+
+let val = useRef()
 function showdata() {
   console.log(val.current.value);
   inputval.push(val.current.value)
@@ -36,37 +54,14 @@ function showdata() {
 
 
 <div>
-   {
-   async function getdata() {
-    try {
+  {wheatherdata ? <div>
+    {wheatherdata.map((items)=>{
+      return <div>
 
-        let data = await axios(`https://api.weatherapi.com/v1/current.json?key=6326e206cf9041e5b3853528241406&q=${val.current.value}&aqi=no`)
-        console.log(data);
-wheatherdata.push(data.data)
-setwhetherdata(wheatherdata)
-console.log(wheatherdata);
-
-// renderdata()
-
-wheatherdata ? wheatherdata.map((items)=>{
-   return <div>
-
-     <h1>{items.location.name}</h1>
-   </div>
- }):console.log('error')
-      }
-     catch (error) {
-      console.log(error);
-      
-    }
-    
-    
-  }
-   
-   
-   
-    }
-    
+        <h1>{items.location.name}</h1>
+      </div>
+    })}
+    </div>:<div><h1>loading</h1></div>}
 </div>
 
 
